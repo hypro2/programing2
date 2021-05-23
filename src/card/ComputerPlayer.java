bc193803d9408c9426663cf0131fd6fef83071b7
HYEONGJUN <HYEONGJUN@192.168.1.178>
2021-05-23 18:48:08 +0900

    딜러테스트05/23/18:48

--- src/card/ComputerPlayer.java
+++ src/card/ComputerPlayer.java
@@ -5,18 +5,19 @@ public class ComputerPlayer extends CardPlayer {
 	public ComputerPlayer(int max_cards) {
 		super(max_cards);
 	}
-	int r = 0;	
+	int card_count = 0;	
 	public boolean wantsACard() {
 		boolean decision2 = false;
-		r+=1;
+		card_count+=1;
 		
+//		int r = 0;
 //		Card[] c = super.showCards();
 //		for(int i =0; i < c.length; i++) {
 //			r += c[i].getRank();
 //			System.out.println(r+" "+i);
 //		}
 		
-		if (r <= 10) {
+		if (card_count <= 10) {
 			decision2 = true;
 		}
 		return decision2;
