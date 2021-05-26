6e8b830bf1048bb915cbbd5084d1b79a23272835
HYEONGJUN <HYEONGJUN@192.168.1.178>
2021-05-26 23:52:55 +0900

    .

--- src/card/Dealer.java
+++ src/card/Dealer.java
@@ -45,10 +45,10 @@ public class Dealer {
 		
 		//½ÂÆÐ 
 		System.out.println("----------½Â/ÆÐ----------");
-		if (h_sumcard == 21 | h_sumcard > ch_sumcard & h_sumcard < 21 & ch_sumcard < 21) {
+		if (h_sumcard == 21 & h_sumcard != ch_sumcard | h_sumcard > ch_sumcard & h_sumcard < 21 & ch_sumcard < 21) {
 			System.out.println("ÀÎ°£ ½Â");
 		}
-		else if (ch_sumcard ==21 | ch_sumcard > h_sumcard & h_sumcard < 21 & ch_sumcard < 21) {
+		else if (ch_sumcard ==21  & h_sumcard != ch_sumcard | ch_sumcard > h_sumcard & h_sumcard < 21 & ch_sumcard < 21) {
 			System.out.println("ÄÄÇ»ÅÍ ½Â");
 		}
 		else if (ch_sumcard > 21 & h_sumcard < 21) {
@@ -59,7 +59,7 @@ public class Dealer {
 			System.out.println("ÀÎ°£ÀÇ burst!!!!");
 			System.out.println("ÄÄÇ»ÅÍ ½Â");
 		}
-		else if(h_sumcard == ch_sumcard | h_sumcard > 21 & ch_sumcard > 21){
+		else{
 			System.out.println("ºñ°å½À´Ï´Ù.");
 		}
 	}
