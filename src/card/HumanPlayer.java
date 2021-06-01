0dfdbdc9b13e1512e34f04ec8769282c3e4af497
김형준 <>
2021-06-02 08:49:48 +0900

    유저 첫번째 카드 드로우

--- src/card/HumanPlayer.java
+++ src/card/HumanPlayer.java
@@ -8,10 +8,9 @@ public class HumanPlayer extends CardPlayer {
 		super(max_cards);
 	}
 	public boolean wantsACard() {
-		String response = JOptionPane.showInputDialog("한장 더 드릴까요? (Y/N)");
-		
 		//리턴 decision1로 변경
-		boolean decision1 = response.equals("Y") || response.equals("y");
+		String response = "Y";
+		boolean decision1 = true;
 		
 		//카드 숫자 합
 		int card_sum = 0;
@@ -19,10 +18,16 @@ public class HumanPlayer extends CardPlayer {
 		for(int i = 0; i < c.length; i++) {
 			card_sum += c[i].getRank();}
 		
+		//첫번째 카드는 필수
+		if (card_sum == 0) {
+			decision1 = true;}
 		//21초과 버스트 출력하고 스탑
-		if(card_sum >= 21) {
+		else if(card_sum >= 21) {
 			decision1 = false;}
-		
+		else
+			response = JOptionPane.showInputDialog("한장 더 드릴까요? (Y/N)");
+			decision1 = response.equals("Y") || response.equals("y");
+
 		return decision1;
 
 	}
