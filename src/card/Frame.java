52999c5a1950eb0948d11c842f0e8cd6fe53459b
HYEONGJUN <HYEONGJUN@192.168.1.178>
2021-06-02 20:54:40 +0900

    ..

--- src/card/Frame.java
+++ src/card/Frame.java
@@ -5,8 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-import fe.MoreCardButton;
-import fe.StopButton;
 
 public class Frame extends JFrame implements ActionListener {
 	
