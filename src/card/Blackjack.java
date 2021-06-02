8a3bfcd1b23ff9fa31372919a947f8dd985a1e25
HYEONGJUN <HYEONGJUN@192.168.1.178>
2021-06-02 21:50:49 +0900

    결과발표

--- src/card/Blackjack.java
+++ src/card/Blackjack.java
@@ -15,7 +15,6 @@ public class Blackjack {
 		d.dealTo(p);
 		System.out.println("---------컴퓨터----------");
 		d.dealTo(c);
-		System.out.println("----------결과----------");
 
 		//카드 패 오픈
 		Card[] h = p.showCards();
@@ -24,14 +23,11 @@ public class Blackjack {
 		ch_sumcard = 0;
 		for (int i = 0; i<h.length;i++ ) {
 			h_sumcard += h[i].getRank();}
-		System.out.println("사람 카드 합 : "+h_sumcard);
 		
 		for (int j = 0; j<ch.length;j++ ) {
 			ch_sumcard += ch[j].getRank();}
-		System.out.println("컴퓨터 카드 합 : "+ch_sumcard);
 				
 		//승패 
-		System.out.println("----------승/패----------");
 		if (h_sumcard == 21 & h_sumcard != ch_sumcard | h_sumcard > ch_sumcard & h_sumcard < 21 & ch_sumcard < 21) {
 			winner = "인간 승";	
 		}
@@ -39,17 +35,14 @@ public class Blackjack {
 			winner = "컴퓨터 승";
 		}
 		else if (ch_sumcard > 21 & h_sumcard < 21) {
-			System.out.println("컴퓨터의 burst!!!!");
 			winner = "인간 승";
 		}
 		else if (h_sumcard > 21 & ch_sumcard < 21) {
-			System.out.println("인간의 burst!!!!");
 			winner = "컴퓨터 승";
 		}
 		else{
 			winner = "비김";
 		}
-		System.out.println(winner);
 	}
 	
 	public int get_Human_sum() {
--- src/card/Frame.java
+++ src/card/Frame.java
@@ -9,21 +9,38 @@ import javax.swing.*;
 public class Frame extends JFrame implements ActionListener {
 	
 	private Blackjack blackjack;
-	private JLabel label =  new JLabel("승자는?");
+	private JLabel label1 =  new JLabel("인간의 카드?");
+	private JLabel label2 =  new JLabel("컴퓨터의 카드?");
+	private JLabel label3 =  new JLabel("승자는?");
 	
 	public Frame(Blackjack b){	
 		blackjack = b; 
 		Container cp = getContentPane();
-		cp.setLayout(new FlowLayout());
-		cp.add(label);
-		cp.add(new Button("결과 발표!", blackjack, this));
+		
+		JPanel north = new JPanel(new FlowLayout());
+		north.add(label1);
+		north.add(label2);
+		
+		JPanel center = new JPanel(new FlowLayout());
+		center.add(label3);
+		
+		JPanel south = new JPanel(new FlowLayout());
+		south.add(new Button("결과 발표!", blackjack, this)); 
+		
+		cp.setLayout(new BorderLayout());
+		cp.add(north, BorderLayout.NORTH);
+		cp.add(center, BorderLayout.CENTER);
+		cp.add(south, BorderLayout.SOUTH);
+		
 		setTitle("Blackjack");
-		setSize(215,100);
+		setSize(215,140);
 		setVisible(true);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 	public void update() {
-		label.setText(blackjack.winner()+"!!!!");
+		label1.setText("인간 : "+blackjack.get_Human_sum());
+		label2.setText("컴퓨터 : "+blackjack.get_Com_sum()+"");
+		label3.setText(blackjack.winner());
 		repaint();
 	}
 	
