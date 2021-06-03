72fac4c2f0cda53b6ac2db8a07064d8bed8fba7e
DMC CONET <DMC CONET@DESKTOP-V6LFV5A>
2021-06-03 17:47:25 +0900

    블랙잭, 휴먼플레이어 버그남

--- src/card/HumanPlayer.java
+++ src/card/HumanPlayer.java
@@ -9,7 +9,7 @@ public class HumanPlayer extends CardPlayer {
 	}
 	public boolean wantsACard() {
 		//리턴 decision1로 변경
-		String response = JOptionPane.showInputDialog("한장 더 드릴까요? (Y/N)");
+//		String response = JOptionPane.showInputDialog("한장 더 드릴까요? (Y/N)");
 		boolean decision1 = true;
 		
 		//카드 숫자 합
@@ -25,8 +25,8 @@ public class HumanPlayer extends CardPlayer {
 		//21초과 버스트 출력하고 스탑
 		else if(card_sum >= 21) {
 			decision1 = false;}
-		else
-			decision1 = response.equals("Y") || response.equals("y");
+//		else
+//			decision1 = response.equals("Y") || response.equals("y");
 
 		return decision1;
 
