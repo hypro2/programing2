ce2928b58924706e74fd7782ff700e0cac8dd23d
DMC CONET <DMC CONET@DESKTOP-V6LFV5A>
2021-06-04 16:36:38 +0900

    .

--- src/card/Blackjack.java
+++ src/card/Blackjack.java
@@ -5,7 +5,9 @@ import java.awt.List;
 import java.util.*;
 
 public class Blackjack {
-	 
+	
+	private CardDeck deck;
+	private Card c;
 	private int h_sumcard;
 	private int ch_sumcard;
 	private int h_count;
@@ -15,8 +17,6 @@ public class Blackjack {
 	private Card[] ch;
 	private String[] list_hand = new String[11];
 	private String[] list_chand= new String[11];
-	private CardDeck deck;
-	private Card c;
 	private boolean stophuman;
 	
 	public Blackjack(int card_num) {
@@ -25,15 +25,16 @@ public class Blackjack {
 		HumanPlayer p = new HumanPlayer(card_num);
 		ComputerPlayer c = new ComputerPlayer(card_num);
 		
-		//시작할때 카드 1장만 줌
-		d.dealTo2(p);
+		//인간은 카드 1장
 		//컴퓨터는 미리 실행
+		d.dealTo2(p);
 		d.dealTo(c);
 		
+		//시작할때 패
 		h = p.showCards();
 		ch = c.showCards();
 
-		//인간 카드
+		//인간 카드를 문자로바꾸고 리스트에 저장
 		h_count=1;
 		h_sumcard=0;
 		for (int i = 0; i<h.length;i++ ) {
@@ -41,7 +42,7 @@ public class Blackjack {
 			list_hand[i] = h[i].getSuit()+" "+h[i].getRank();
 			}
 		
-		//컴퓨터 카드
+		//컴퓨터 카드를 문자로 바꾸고 리스트에 저장
 		ch_count=ch.length;
 		ch_sumcard = 0;
 		for (int j = 0; j<ch.length;j++ ) {
@@ -49,73 +50,59 @@ public class Blackjack {
 			list_chand[j] = ch[j].getSuit()+" "+ch[j].getRank();
 				}
 	
-		//사람이 그만둘때 드로잉,프레임에 활용
+		//사람이 그만둘때 컴퓨터터 카드를 보이게 하기위해 드로잉,프레임에 활용
 		stophuman = true;	
 		}
 
 	
-	//인간 딜러 역할 한장씩 뽑아서 추가함
+	//인간 딜러 역할 한장씩 뽑아서 추가함(버튼에서 활용)
+	//매번 새 카드덱에서 뽑아서 중복이 걸릴수 있음!!!
 	public void hit(){
 		deck = new CardDeck();
 		c = deck.newCard(); 		
 		if (h_sumcard<21 & stophuman) {
 			h_sumcard = h_sumcard + c.getRank();		
 			list_hand[h_count()]=c.getSuit()+" "+c.getRank();
-			h_count +=1;
-		}		
-	}
+			h_count +=1;}		
+		}
 	
 	//카드패 리턴
 	public String[] list_hand() {
-		return list_hand;
-	}
+		return list_hand;}
 	public String[] list_chand() {
-		return list_chand;
-	}
+		return list_chand;}
 		
-	//스톱버튼을 누르면 스톱휴먼이 펄스가됨
-	public void stophuman() {
-		stophuman = false;
-	}
-	
+	//스톱버튼을 누르면 스톱휴먼이 펄스가됨(버튼에서 활용)
 	//스톱휴먼의 값을 리턴해줌
+	public void stophuman() {
+		stophuman = false;}
 	public boolean stop() {
-		return stophuman;
-	}
+		return stophuman;}
 	
+	//카드 매수 리턴
 	public int h_count() {
-		return h_count;
-	}
-	
+		return h_count;}
 	public int ch_count() {
-		return ch_count;
-	}
+		return ch_count;}
 	
-	// 카드패, 카드합 리턴
+	//카드합 리턴
 	public int HumanSumCard() {
-		return h_sumcard;
-	}
+		return h_sumcard;}
 	public int ComSumCard() {
-		return ch_sumcard;
-	}
+		return ch_sumcard;}
 	
 	//승자 리턴
 	public String winner() {
 		if (h_sumcard == 21 & h_sumcard != ch_sumcard | h_sumcard > ch_sumcard & h_sumcard < 21 & ch_sumcard < 21) {
-			winner = "인간 승";	
-		}
+			winner = "인간 승";}
 		else if (ch_sumcard ==21  & h_sumcard != ch_sumcard | ch_sumcard > h_sumcard & h_sumcard < 21 & ch_sumcard < 21) {
-			winner = "컴퓨터 승";
-		}
+			winner = "컴퓨터 승";}
 		else if (ch_sumcard > 21 & h_sumcard < 21) {
-			winner = "인간 승";
-		}
+			winner = "인간 승";}
 		else if (h_sumcard > 21 & ch_sumcard < 21) {
-			winner = "컴퓨터 승";
-		}
+			winner = "컴퓨터 승";}
 		else{
-			winner = "비김";
-		}
+			winner = "비김";}
 		return winner;
 	}
 
--- src/card/Drawing.java
+++ src/card/Drawing.java
@@ -20,16 +20,18 @@ public class Drawing extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		
-		
+		//배경색
 		g.setColor(Color.white);
 		g.fillRect(0, 0, 500, 400);
 		
+		//인간 카드
 		g.setColor(Color.red);
 		g.drawString("인간 카드",10,10);		
-		
+		//폭사 조건
 		if ( blackjack.HumanSumCard() >21) {
 			g.drawString("인간의 Burst!!!!",10,100);}
-			
+		
+		//인간 카드그리기	
 		int x = 0;
 		list_hand = blackjack.list_hand();
 		for (int i = 0; i <blackjack.h_count() ; i++) {
@@ -38,9 +40,12 @@ public class Drawing extends JPanel {
 			x+=1;
 			}
 		
+		//컴퓨터 카드그리기
 		g.setColor(Color.blue);
 		g.drawString("컴퓨터 카드",10,120);
 		list_chand = blackjack.list_chand();
+		
+		//스톱버튼을 누르면 공개되게 해둠
 		if (!blackjack.stop()) {	
 			int y = 0;
 			for (int i = 0; i <blackjack.ch_count() ; i++) {
@@ -48,24 +53,23 @@ public class Drawing extends JPanel {
 				g.drawRect(y*43+3, 125, 41, 70);
 				y+=1;
 				}
-			
+			//컴퓨터 폭사 조건
 			if ( blackjack.ComSumCard() >21) {
 				g.drawString("컴퓨터의 Burst!!!!",10,210);
 				}
 			
+			//승자 폰트
 			Font font = g.getFont().deriveFont( 20.0f );
 		    g.setFont( font );
 		    if (blackjack.winner()=="컴퓨터 승") {
-		    	g.setColor(Color.blue);
-		    	}
+		    	g.setColor(Color.blue);}
 		    else if (blackjack.winner()=="인간 승") {
-		    	g.setColor(Color.red);
-		    	}
+		    	g.setColor(Color.red);}
 		    else {
-		    	g.setColor(Color.orange);
-		    	}
-	    	g.drawString("승자 : "+ blackjack.winner(), 150, 240); 
-				}
+		    	g.setColor(Color.orange);}
+	    	g.drawString("승자 : "+ blackjack.winner(), 150, 240); }
+		
+		//스톱버튼 누르기 전까지 비밀
 		else {
 			g.drawString("비밀",10,160);
 			}
